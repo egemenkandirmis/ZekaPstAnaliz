8cfbd6f2bafbd1a17afeba0d9cb50e8c201da026
Egemen Kandirmis <kandirmis@outlook.com>
2021-08-23 17:40:45 +0300

    Veriler JSON dosya formatına aktarıldı. JSON dosyasını okuyup excel'e aktaran method yazıldı

---
+++ MailBackupSystem/MailBackupSystem/Mail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailBackupSystem
+{
+    public class Mail
+    {
+        public DateTime DeliveryTime { get; set; }
+        public string SenderEmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string DisplayTo { get; set; }
+        public string DisplayBcc { get; set; }
+        public string DisplayCc { get; set; }
+        public string Body { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
